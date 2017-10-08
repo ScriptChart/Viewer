993f2858a4414a8b06b83bea4f4fbc55887b4aba
Alexey Paramonov <alex.s.paramonov@gmail.com>
2017-10-08 13:52:02 +0200

    Fixed deserilization issues. fixes #22

--- ScriptChartViewer/Controllers/HomeController.cs
+++ ScriptChartViewer/Controllers/HomeController.cs
@@ -11,22 +11,49 @@ namespace WebChartViewer.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ChartIdModel chartIdModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return ChartViewById(chartIdModel.ChartId);
+            }
+            return View();
+        }
+
         // /2gWy0MzimOLR0Qp47Qr
         [HttpGet("{chartId}")]
-        public IActionResult Index(string chartId)
+        public IActionResult ChartViewById(string chartId)
         {
+            ViewData["DataRows"] = QueryDataByChartId(chartId).GetAwaiter().GetResult();
+            ViewData["TestData"] = chartId;
+            return View("ChartViewById");
+        }
+
+        private async Task<string> QueryDataByChartId(string chartId)
+        {
+            string url = "http://88.99.187.144";
+#if DEBUG
+            url = "http://localhost:21702";
+#endif
+            string result;
             using (var httpclient = new HttpClient())
             {
                 httpclient.BaseAddress =
-                    new Uri("http://88.99.187.144");
+                    new Uri(url);
                 var resp = httpclient.GetAsync($"/api/linechart/{chartId}").GetAwaiter().GetResult();
                 resp.EnsureSuccessStatusCode();
 
-                ViewData["DataRows"] = resp.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                result = await resp.Content.ReadAsStringAsync();
             }
 
-            ViewData["TestData"] = chartId;
-            return View();
+            return result;
         }
 
         public IActionResult Error()
---
+++ ScriptChartViewer/Models/ChartIdModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptChartViewer.Models
+{
+    public class ChartIdModel
+    {
+        [Required]
+        public string ChartId { get; set; }
+    }
+}
